6a86c3e6a934c27ee12db509c9eb8960ecb84fce
Christina Sarai Atoche Pascual <atochepascual.saraichristina@gmail.com>
2024-07-25 15:45:24 -0500

    build: crea tareas en gulp para optimización en css

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,22 @@ const autoprefixer = require('autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const cssnano = require('cssnano');
 
+//IMAGENES
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+// FONT
+const ttf2woff = require('gulp-ttf2woff');
+const ttf2woff2 = require('gulp-ttf2woff2');
+
+
 
 function css(done) {
     src('./src/scss/app.scss')
         .pipe(sourcemaps.init())
         .pipe(sass())
-        .pipe(postcss([autoprefixer(), cssnano()]))
+        // .pipe(postcss([autoprefixer(), cssnano()]))
         .pipe(postcss([autoprefixer()]))
         .pipe(sourcemaps.write('.'))
         .pipe( dest('./build/css'))
@@ -20,5 +30,67 @@ function css(done) {
     done()        
 }
 
+function imagen(done) {
+    src('./src/img/**/*')
+        .pipe(imagemin({optimizationLevel: 3}))
+        .pipe(dest('./build/img'))
+
+    done()        
+}
+
+function versionWebp(done) {
+    src('./src/img/**/*.{jpg,png}')
+        .pipe(webp())
+        .pipe(dest('./build/img'))
+
+    done()        
+}
+
+function versionAvif(done) {
+    src('./src/img/**/*.{jpg,jpeg,png}')
+        .pipe(avif())
+        .pipe(dest('build/img'));
+
+    done();        
+}
+
+function font(done) {
+    src('./src/font/**/*')
+        .pipe(dest('./build/font'))
+
+    done()
+}
+
+function woff(done) {
+    src('./src/font/**/*')
+        .pipe(ttf2woff())
+        .pipe(dest('./build/font'))
+
+    done()        
+}
+
+function woff2(done) {
+    src('./src/font/**/*')
+        .pipe(ttf2woff2())
+        .pipe(dest('./build/font'))
+
+    done()        
+}
+
+
+function dev(done) {
+    watch('./src/scss/**/*.scss', css)
+    watch('./src/img/**/*', imagen)
+
+    done()
+}
+
 exports.css = css;
 exports.dev = dev;
+exports.imagen = imagen;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.font = font;
+exports.woff = woff;
+exports.woff2 = woff2;
+exports.default = series(imagen, versionWebp, versionAvif, font, woff, woff2, css, dev);
